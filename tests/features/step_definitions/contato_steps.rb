7492a93d61cbbf5140860dc5e473d8843bab2b4b
Diego Barros <diego.barrosfrc@gmail.com>
2019-05-27 00:41:54 -0300

    metade da aula dois

--- tests/features/step_definitions/contato_steps.rb
+++ tests/features/step_definitions/contato_steps.rb
@@ -3,14 +3,24 @@ Dado('que {string} é meu novo contato') do |nome|
 end
 
 Dado('este contato possui os seguintes dados:') do |table|
-  # table is a Cucumber::MultilineArgument::DataTable
-  pending # Write code here that turns the phrase above into concrete actions
+ @dados_contato = table.rows_hash
 end
 
 Quando('faço o cadastro deste novo contato') do
   visit 'http://localhost:8080/form-novo.html'
+
+  #fill_in só funciona com 'id'
+  fill_in 'nome', with: @nome
+  #find('#nome').set @nome
+  fill_in 'email', with: @dados_contato[:email]
+  fill_in 'celular', with: @dados_contato[:celular]
+
+  tipo_contato = find('#tipo')
+  tipo_contato.find('option', text: @dados_contato[:tipo]).select_option
+
+  click_on 'Cadastrar'
 end
 
 Então('devo ver a seguinte mensagem de sucesso {string}') do |mensagem|
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(page).to have_content mensagem
 end
